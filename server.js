d6f2eb087179a8b70cb21febc5c5a41dbf915f68
Hall Family <charliesamhall@icloud.com>
2024-02-27 12:55:39 -0500

    created connection and server js files, created model seed and route folders

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+// const routes = require('./routes')
+const sequelize = require('./config/connection')
+
+const app = express()
+const PORT = 3001
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// app.use(routes)
+
+const syncDB = async () => {
+    try {
+        sequelize.sync({ force: false })
+        .then(app.listen(PORT, () => {
+            console.log(`App listening on port ${PORT}!`)
+        }))
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+syncDB()
