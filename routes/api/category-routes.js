9473b04f6c9c703ec722ab11205be0cc11c1aab2
Hall Family <charliesamhall@icloud.com>
2024-02-27 21:29:10 -0500

    added get routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,25 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{model: Product}]
+    })
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(404).json(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
+    })
+  } catch (err) {
+    res.status(404).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
