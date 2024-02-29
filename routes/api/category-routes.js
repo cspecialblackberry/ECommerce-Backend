cc4107b90670e9e2a7daa47cf6c227c9136bf35c
Hall Family <charliesamhall@icloud.com>
2024-02-29 12:33:34 -0500

    added category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,21 +19,45 @@ router.get('/:id', async (req, res) => {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{model: Product}]
     })
+    res.status(200).json(categoryData)
   } catch (err) {
     res.status(404).json(err)
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(404).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try{
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json(categoryData)
+  }catch (err) {
+    res.status(404).json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async(req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(404).json(err)
+  }
 });
 
 module.exports = router;
