b9fda46c6b4191b6a9a4cadb6e104f7e4fc13098
Hall Family <charliesamhall@icloud.com>
2024-02-29 14:53:10 -0500

    added delete product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -104,8 +104,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productTag = await ProductTag.destroy({
+      where: {
+        product_id: req.params.id
+      }
+    })
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json(tagData)
+  } catch (err) {
+    res.status(404).json(err)
+  }
 });
 
 module.exports = router;
